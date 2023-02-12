edd4ca5d2c53c14c8ccc99cbc8af5cc7ccf3b772
yatharth <yatharthverma070@gmail.com>
2023-02-12 10:27:14 +0530

    feat: Add logic to show subscribed content

--- pages/[id].js
+++ pages/[id].js
@@ -1,11 +1,30 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import { supabase } from "../utils/supabase";
+import Video from "react-player";
 
 const LessonDetails = ({ lesson }) => {
-  console.log(lesson);
+  const [premiumUrl, setPremiumUrl] = useState("");
+
+  useEffect(() => {
+    getPremiumContent();
+  });
+
+  const getPremiumContent = async () => {
+    const { data: premiumContentRecord } = await supabase
+      .from("premium_content")
+      .select("premium_url")
+      .eq("id", lesson?.id)
+      .single();
+
+    setPremiumUrl(premiumContentRecord?.premium_url);
+  };
+
   return (
     <div className="w-full max-w-3xl mx-auto py-16 px-8">
       <h1 className="text-3xl mb-6">{lesson.title}</h1>
-      <p>{lesson.description}</p>
+      <p className="mb-4">{lesson.description}</p>
+      <p>{premiumUrl && <Video url={premiumUrl} width="100" />}</p>
     </div>
   );
 };
